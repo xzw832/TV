536e7966c63c3e464df17efc57309f25d10f6a9b
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-10 22:31:33 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,11 @@
 source_file = "demo.txt"
 final_file = "result.txt"
 favorite_list = [
-    "珠江",
-    "开平综合",
-    "开平生活",
-    "CCTV1",
-    "CCTV5",
-    "CCTV5+",
-    "CCTV13",
-    "广东体育",
-    "广东卫视",
-    "大湾区卫视",
-    "浙江卫视",
-    "湖南卫视",
-    "翡翠台",
+    "凤凰",
+    "汕头",
 ]
-favorite_page_num = 5
-default_page_num = 3
+favorite_page_num = 1
+default_page_num = 1
 urls_limit = 15
 response_time_weight = 0.5
 resolution_weight = 0.5
